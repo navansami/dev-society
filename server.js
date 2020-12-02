af8a7ec328745d32bde9457fa50a595acf8dc087
Navan Sami <navan.sami@yahoo.com>
2020-12-02 08:51:33 +0200

    fixed typo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send('Welcome to my server');
+  res.send('Backend - Dev Society');
 });
 
 const PORT = process.env.PORT || 5000;
